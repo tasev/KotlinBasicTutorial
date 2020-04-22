21c8ddf8c64a525c87f41e370fc11d71f2637849
tasev <tase.tasev@codetri.be>
2020-04-22 18:38:15 +0200

    commit test

--- app/src/main/java/com/gsixacademy/android/kotlinbasictutorial/MainActivity.kt
+++ app/src/main/java/com/gsixacademy/android/kotlinbasictutorial/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     var listActivities: ArrayList<String> = arrayListOf("RelativeLayoutActivity","CalculatorActivity","TodoListActivity")
 
+    //this is for commit test only
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
